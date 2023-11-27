8d3ba68794adfbc1ce14498b801c53f67ccf2d5e
rajeshkumar1130 <74873530+rajeshkumar1130@users.noreply.github.com>
2023-11-27 14:56:37 +0530

    Add project files.

---
+++ CubeAlgoTest/wwwroot/src/cube.js
@@ -0,0 +1,193 @@
+import { useState, useEffect, useRef, html, css } from './utils.js';
+import { interpret } from './utils.js';
+
+export default (props) => {
+  const container = useRef(null);
+  const cube = useRef(new ERNO.Cube());
+  const [speed, setSpeed] = useState(200);
+
+  const centerCube = () =>
+    cube.current.rotation.set((30 * Math.PI) / 180, (-45 * Math.PI) / 180, 0);
+  useEffect(() => {
+    if (props.case) {
+      const hist = cube.current.twistQueue.history
+        .map((x) =>
+          x.command === x.command.toLowerCase()
+            ? x.command.toUpperCase()
+            : x.command.toLowerCase()
+        )
+        .join('');
+
+      cube.current.twistDuration = 50;
+      cube.current.autoRotate = false;
+      centerCube();
+        if (hist.length > 0) cube.current.shuffle(hist);
+        cube.current.twist(interpret(props.case.algs[0], true));
+    }
+  }, [props.case]);
+
+    useEffect(() => {
+    container.current.appendChild(cube.current.domElement);
+    cube.current.verbosity = 0;
+    cube.current.autoRotate = !props.case;
+  }, []);
+
+  return html`
+    <div className=${style.container} onDoubleClick=${centerCube}>
+      <div className=${style.canvas} ref=${container}></div>
+      ${props.case &&
+      html`
+        <h1
+          className=${style.alg}
+          onClick=${(e) => {
+            cube.current.twistDuration = speed;
+            cube.current.twist(interpret(props.case.algs[0]));
+          }}
+        >
+          ${props.case.algs[0]}
+        </h1>
+        <p className=${style.group}>${props.case.group}</p>
+        <input
+          className=${style.slider}
+          key="speed"
+          type="range"
+          defaultValue=${speed}
+          step="100"
+          min="0"
+          max="900"
+          onChange=${(e) => {
+            cube.current.twistDuration = e.target.value;
+            setSpeed(e.target.value);
+          }}
+        />
+        <div className=${style.controls}>
+          <button
+            onClick=${(e) => {
+              cube.current.twistDuration = speed;
+              cube.current.twist(interpret(props.case.algs[0], true));
+            }}
+          >
+            Reverse
+          </button>
+          <button onClick=${centerCube}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path fill="none" d="M0 0h24v24H0V0z" />
+              <path
+                d="M12 4C7 4 2.73 7.11 1 11.5 2.73 15.89 7 19 12 19s9.27-3.11 11-7.5C21.27 7.11 17 4 12 4zm0 12.5c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"
+              />
+            </svg>
+          </button>
+          <button
+            onClick=${(e) => {
+              cube.current.twistDuration = speed;
+              cube.current.twist(interpret(props.case.algs[0]));
+            }}
+          >
+            Execute
+          </button>
+            <button
+            onClick=${(e) => {
+              debugger;
+              const hist = cube.current.twistQueue.history
+                  .map((x) =>
+                      x.command === x.command.toLowerCase()
+                          ? x.command.toUpperCase()
+                          : x.command.toLowerCase()
+                  )
+                  .join('');
+
+              cube.current.twistDuration = 50;
+              cube.current.autoRotate = false;
+              centerCube();
+              if (hist.length > 0) cube.current.shuffle(hist);
+              cube.current.twist(interpret(props.case.algs[0], true));
+
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      `}
+    </div>
+  `;
+};
+
+const style = {
+  container: css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: calc(var(--vh, 1vh) * 100);
+    background: radial-gradient(#222, #111);
+  `,
+  canvas: css`
+    width: calc(var(--vh, 1vh) * 55);
+    height: calc(var(--vh, 1vh) * 55);
+    transform: translateY(-5%);
+    animation: bob 3s infinite;
+    overflow: hidden;
+    &:only-child {
+      width: 70vmin;
+      height: 70vmin;
+      animation: none;
+    }
+    @keyframes bob {
+      from: {
+        transform: translateY(-5%);
+      }
+      50% {
+        transform: translateY(-6%);
+      }
+      to {
+        transform: translateY(-5%);
+      }
+    }
+  `,
+  alg: css`
+    font-size: calc(14px + 2vmin);
+    color: rgba(255, 255, 255, 0.8);
+    text-align: center;
+    padding: 0 2rem;
+    line-height: 162%;
+    cursor: pointer;
+    margin-bottom: 1rem;
+  `,
+  controls: css`
+    display: flex;
+
+    button {
+      font-size: calc(10px + 1vmin);
+      text-transform: uppercase;
+      letter-spacing: 0.2em;
+      background: #121212;
+      padding: 1vmin 2vmin;
+      color: rgba(255, 255, 255, 0.38);
+      font-weight: bold;
+      svg {
+        width: calc(24px + 2vmin);
+        height: calc(24px + 2vmin);
+        fill: currentColor;
+      }
+    }
+    button + button {
+      margin-left: 1rem;
+    }
+  `,
+  slider: css`
+    margin: 1rem 0rem 1rem;
+    max-width: 40rem;
+    width: 90%;
+  `,
+  group: css`
+    font-size: calc(10px + 1vmin);
+    color: #555;
+    text-transform: uppercase;
+    letter-spacing: 0.1em;
+  `,
+};
---
+++ CubeAlgoTest/wwwroot/src/nav.js
@@ -0,0 +1,107 @@
+import { useState, html, css } from './utils.js';
+
+const imageForCase = (alg) => {
+  let x = alg.replace(/ /g, '');
+  if (alg.startsWith("x' ")) x = x + ' x';
+  if (alg.startsWith('x ')) x = x + " x'";
+    return `http://cube.rider.biz/visualcube.php?fmt=svg&size=300&bg=t&sch=fdcc0a,dc422f,3d80f6,ffffff,ff6c00,009d53&case=${x}`;
+};
+
+const item = (route, go, filterType) => (move, index) => html`
+  <a
+    data-hidden=${filterType === '' ? false : move.type !== filterType}
+    data-active=${route == index}
+    key=${move.algs[0]}
+    onClick=${(e) => {
+      e.preventDefault();
+      go(index);
+      window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+    }}
+  >
+    <img id=${move.name} src=${imageForCase(move.algs[0])} />
+    <p>${move.name}</p>
+  </a>
+`;
+
+export default (props) => {
+  const [filterType, setFilterType] = useState('oll');
+  console.log(filterType);
+    return html`
+    <nav className=${style.nav}>
+      <div className=${style.filters}>
+        <button onClick=${(e) => setFilterType('f2l')}>f2l</button>
+        <button onClick=${(e) => setFilterType('oll')}>oll</button>
+        <button onClick=${(e) => setFilterType('pll')}>pll</button>
+      </div>
+      <div className=${style.links}>
+        ${props.links.map(item(props.route, props.go, filterType))}
+      </div>
+    </nav>
+  `;
+};
+
+const style = {
+  nav: css`
+    @media (orientation: landscape) {
+      overflow-y: auto;
+    }
+    background: #222;
+    height: 100vh;
+    padding: 5vmin;
+    > * + * {
+      margin-top: 2rem;
+    }
+    opacity: 0.62;
+    transition: opacity 0.62s;
+    &:hover {
+      opacity: 1;
+    }
+  `,
+  filters: css`
+    width: 100%;
+    display: flex;
+    > * + * {
+      border-left: 1px solid rgba(0, 0, 0, 0.8);
+    }
+    button {
+      flex: 1 1 100%;
+      padding: 1rem 0;
+      text-transform: uppercase;
+      font-weight: bold;
+    }
+  `,
+  links: css`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(110px, 1fr));
+    grid-gap: 0.62rem;
+    [data-hidden='true'] {
+      display: none;
+    }
+    > a {
+      width: 100%;
+      padding-top: 100%;
+      &[data-active='true'] > img {
+        opacity: 0.3;
+        transform: scale(0.9);
+      }
+    }
+    img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      opacity: 0.8;
+      transition: transform 0.2s, opacity 0.3s;
+      &:hover {
+        opacity: 1;
+        transform: scale(1.038);
+      }
+    }
+    p {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      color: #555;
+    }
+  `,
+};
